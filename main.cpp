91156ad985b8913626275eec57a3d14a7b81716e
Josh Arms <josharms@Joshs-MacBook-Pro.local>
2019-02-20 08:41:00 -0600

    Make it more accurate
    Approximations can be made more or less accurate by changing the const ERROR varrible at the top.

--- main.cpp
+++ main.cpp
@@ -4,34 +4,39 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int sqrt(int);
+double sqrt(int);
+
+const float ERROR = .00001;
 
 int main(){
   cout << "This program estimates the square root of a number without using cmath's sqrt" <<endl;
   int num = 0;
   cout << endl << "Enter a number to check: ";
   cin >> num;
-  int ans = sqrt(num);
-  cout << "The square root is at least: " << ans <<endl;
-  cout << "and at most: " << ans+1<<endl;
+  double ans = sqrt(num);
+  cout << "The square root is: " << ans <<endl;
   return 0;
 }
 
-int sqrt(int num){
+double sqrt(int num){
   if(num*num==num){ //this catches 1 and 0
     return num;
   }
-  int upper = num/2;
-  int lower = 0;
-  int mid = upper/2;
+  int comparisons = 0;
+  double upper = num/2;
+  double lower = 0;
+  double mid = upper/2;
   while(lower<=upper){
+    comparisons++;
+    cout << comparisons;
     mid = (lower+upper)/2;
+    cout << " - " << lower << " - " << upper << " - " << mid << endl;
     if((mid*mid)==num){
       return mid;
     }else if((mid*mid)>num){
-      upper = mid-1;
+      upper = mid-ERROR;
     }else{
-      lower = mid+1;
+      lower = mid+ERROR;
     }
   }
   return mid;
